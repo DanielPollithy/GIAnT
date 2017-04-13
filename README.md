1aed22f6fd0d1871fd4997e60b1f5a93fa4523c5
Daniel Pollithy <daniel.pollithy@gmail.com>
2017-04-14 01:36:30 +0200

    readthedocs badge and neo4j travis

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TransliterationApplication
 [![Build Status](https://travis-ci.org/DanielPollithy/TransliterationApplication.svg?branch=master)](https://travis-ci.org/DanielPollithy/TransliterationApplication)
+
+[![Build Status](https://readthedocs.org/projects/transliterationapplication/badge/?version=latest)](http://transliterationapplication.readthedocs.io/en/latest/?badge=latest)
--- install_neo4j.sh
+++ install_neo4j.sh
@@ -9,10 +9,10 @@ tar zxf $TARBALL
 
 cd "neo4j-community-$VERSION"
 
-sudo sed -i.bak s/#dbms.security.auth_enabled=false/dbms.security.auth_enabled=false/g "neo4j-community-$VERSION/conf/neo4j.conf"
+#sudo sed -i.bak s/#dbms.security.auth_enabled=false/dbms.security.auth_enabled=false/g "neo4j-community-$VERSION/conf/neo4j.conf"
 
-#./bin/neo4j-admin set-initial-password 1234
+./bin/neo4j-admin set-initial-password 1234
 
-./bin/neo4j start
+#./bin/neo4j start
 sleep 5
 
