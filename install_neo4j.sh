130a00c47792c0c614e5246a62ce92bcb83fdb6c
Daniel Pollithy <daniel.pollithy@gmail.com>
2017-04-14 01:43:09 +0200

    neo4j travis

--- install_neo4j.sh
+++ install_neo4j.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-VERSION="3.0.3"
-TARBALL="neo4j3.0.tar.gz"
+VERSION="3.1.3"
+TARBALL="neo4j3.1.3.tar.gz"
 
 cd /tmp
 wget -O $TARBALL "http://dist.neo4j.org/neo4j-community-$VERSION-unix.tar.gz?edition=community&version=$VERSION&distribution=tarball&dlid=2803678"
@@ -11,7 +11,7 @@ cd "neo4j-community-$VERSION"
 
 #sudo sed -i.bak s/#dbms.security.auth_enabled=false/dbms.security.auth_enabled=false/g "neo4j-community-$VERSION/conf/neo4j.conf"
 
-./bin/neo4j-admin set-initial-password 1234
+./bin/neo4j-admin set-initial-password "1234"
 
 #./bin/neo4j start
 sleep 5
