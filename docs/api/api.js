d49293b689343024c4718559749fc5efdde0742e
Daniel Pollithy <daniel.pollithy@gmail.com>
2017-04-28 16:06:36 +0200

    Adds yuidoc

---
+++ docs/api/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Database"
+    ],
+    "modules": [
+        "Database"
+    ],
+    "allModules": [
+        {
+            "displayName": "Database",
+            "name": "Database",
+            "description": "Database wrapper functions for Neo4J"
+        }
+    ],
+    "elements": []
+} };
+});
